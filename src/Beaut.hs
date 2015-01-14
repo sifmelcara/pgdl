89407df86bde85bebc5d576e4cffdaa9d6570f34
sifmelcara <rbdn@9mm.davy.tw>
2015-01-14 20:08:46 +0800

    improve string split algo.

--- src/Beaut.hs
+++ src/Beaut.hs
@@ -6,20 +6,17 @@ import Data.List
 import qualified Data.Text as T
 
 beaut :: (T.Text, T.Text, T.Text) -> T.Text
-beaut (str, _, sz)
+beaut (str, _, _)
     | length dat < 3 = "\n" `app` str
     | otherwise      = T.intercalate "\n" [tb 4 `app` sbt, 
                                            tb 10 `app` nme, 
                                            tb 30 `app` eps
                                           ]
-    where dat =  map cuth . T.splitOn "]" $ str
+    where dat = spl $ str
           (sbt:nme:eps:_) = dat
           tb n = T.replicate n " "
-          cuth str
-            | T.null str = str
-            | T.head str == '[' = T.tail str
-            | otherwise = str
           app s1 s2 = s1 `T.append` s2
+          spl = filter (\t -> T.length t /= 0) . concat . map (T.splitOn "[") . (T.splitOn "]")
 
 
 
