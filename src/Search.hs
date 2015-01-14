fbf12db6ebbeaf32309023c43ee6cf2a557ccf2a
sifmelcara <rbdn@9mm.davy.tw>
2015-01-14 18:19:15 +0800

    case insensitive search

--- src/Search.hs
+++ src/Search.hs
@@ -2,12 +2,13 @@
 
 module Search where
 
-import Data.List
 import qualified Data.Text as T
+import Data.Function
 
 search :: [(T.Text, T.Text, T.Text)] -> [String] -> [(T.Text, T.Text, T.Text)]
-search vlst plst = foldl (\vs ps -> filter (T.isInfixOf ps . getName) vs) vlst pt
+search vlst plst = foldl (\vs ps -> filter (targ ps . getName) vs) vlst pt
     where pt = map T.pack plst
           getName (s, _, _) = s
+          targ = T.isInfixOf `on` T.toUpper
 
 
