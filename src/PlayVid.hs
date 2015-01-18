22aac38b209632a4bdb38e7a4cfc04c3b3ccaa9d
sifmelcara <rbdn@9mm.davy.tw>
2015-01-18 18:46:37 +0800

    use System.Directory.removeFIle instead rm

--- src/PlayVid.hs
+++ src/PlayVid.hs
@@ -6,6 +6,7 @@ import Getconfig
 
 import System.Process
 import System.Directory
+import System.FilePath.Posix
 import Control.Monad
 import qualified Data.Text as T
 import System.IO
@@ -13,13 +14,15 @@ import Video
 
 playVid :: Video -> IO ()
 playVid vid = do
-    runCommand $ "rm " ++ addq vn
-
     username <- fmap T.unpack getUsername
     password <- fmap T.unpack getPassword
     servpath <- fmap T.unpack getServpath
     localdir <- fmap T.unpack getLocaldir
-    let localloc = localdir ++ vn
+
+    let localloc = localdir </> vn
+    fex <- doesFileExist localdir
+    when fex $ removeFile localloc
+
     let purepath = reverse . dropWhile (/= '/') . reverse $ servpath
     let url = "http://" ++ username ++ ":" ++ password ++ "@" ++ purepath ++ vu
     runCommand $ "nohup curl " ++ addq url ++ " -o " ++ addq localloc ++ "&>/dev/null &"
