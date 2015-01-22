cff0df0aa92418cd4d1f453d8c208b39efc953e1
sifmelcara <rbdn@9mm.davy.tw>
2015-01-23 05:00:10 +0800

    clean syntax

--- src/Main.hs
+++ src/Main.hs
@@ -31,8 +31,6 @@ main = do
     lst <- newList 3 
     addToList lst testv =<< (plainText $ beaut testv)
 
-    -----preprocess fileExist-----
-    locdir <- getLocaldir
     ------generate ui----------
     lui <- centered =<< hFixed 80 lst
     dui <- centered =<< hFixed 30 (dialogWidget dlg)
@@ -48,44 +46,40 @@ main = do
     chgls <- addToCollection c lui lfg
     chgdl <- addToCollection c dui dfcg
     
-    ---------create infor screen-----------
-
     -------exit when q is pressed-------
     lfg   `onKeyPressed` (\_ key _ -> if key == KChar 'q' then exitSuccess else return False)
     dfcg  `onKeyPressed` (\_ key _ -> if key == KChar 'q' then exitSuccess else return False)
 
     ------define activate process--------
-    
                                             
     dlg `onDialogCancel` const exitSuccess
-    dlg `onDialogAccept` (\_ ->  do
-                             Just (_, (itm, _)) <- getSelected lst
-                             playVid itm
-                         )
-
+    onDialogAccept dlg $ \_ ->  do
+        Just (_, (itm, _)) <- getSelected lst
+        playVid itm
+                         
     ---------lst--------
-    lst `onKeyPressed` (\_ key _ -> case key of
-                            KEnter -> do
-                                Just (_, (itm, _)) <- getSelected lst
-                                fex itm >>= \case 
-                                    True  -> chgdl
-                                    False -> playVid itm
-                                return True
-                            _      -> return False
-                       )
+    onKeyPressed lst $ \_ key _ -> case key of
+        KEnter -> do
+            Just (_, (itm, _)) <- getSelected lst
+            fex itm >>= \case 
+                True  -> chgdl
+                False -> playVid itm
+            return True
+        _      -> return False
+
     ifsfg <- newFocusGroup
     ifsfg `onKeyPressed` (\_ key _ -> if key == KLeft then chgls >> return True else return False)
     ifsfg `onKeyPressed` (\_ key _ -> if key == KChar 'q' then exitSuccess else return False)
 
-    lst `onKeyPressed` (\_ key _ -> case key of
-                            KRight -> do
-                                Just (_, (itm, _)) <- getSelected lst
-                                inf <- centered =<< (plainText $ crtInfPg itm)
-                                addToFocusGroup ifsfg inf
-                                chgif <- addToCollection c inf ifsfg
-                                chgif
-                                return True
-                            _ -> return False)
+    onKeyPressed lst $ \_ key _ -> case key of
+        KRight -> do
+            Just (_, (itm, _)) <- getSelected lst
+            inf <- centered =<< (plainText $ crtInfPg itm)
+            addToFocusGroup ifsfg inf
+            chgif <- addToCollection c inf ifsfg
+            chgif
+            return True
+        _ -> return False
 
     onSelectionChange lst $ \sle -> case sle of
         SelectionOn _ itm _ -> fex itm >>= \case 
@@ -121,7 +115,7 @@ crtInfPg vid = T.unlines ["", "",
                           "File date: " `app` vidDate vid,
                           "File link: " `app` (spl . vidLink) vid
                          ]
-               where app = T.append
-                     spl = T.intercalate "\n" . T.chunksOf 60 
+    where app = T.append
+          spl = T.intercalate "\n" . T.chunksOf 60 
 
 
