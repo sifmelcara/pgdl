1c4ff83d691f51f3c3df557607dadb0f879c5146
sifmelcara <rbdn@9mm.davy.tw>
2015-01-23 04:42:12 +0800

    It compiles Let's ship it

--- src/Main.hs
+++ src/Main.hs
@@ -25,21 +25,19 @@ import Control.Concurrent
 main = do
     chkcfg
 
-    let vdlst = return $ Video {vidName = "ffff", vidLink = "==++--", vidSize="",vidDate="44"}
+    let testv = Video {vidName = "ffff", vidLink = "==++--", vidSize="",vidDate="44"}
     ------define widgets-----------
     (dlg, dfcg) <- flip newDialog "File Exists!" =<< plainText "redownload it?"
-    lst <- newTextList (map beaut vdlst) 3
+    lst <- newList 3 
+    addToList lst testv =<< (plainText $ beaut testv)
 
     -----preprocess fileExist-----
     locdir <- getLocaldir
     let schg sle = case sle of 
-                SelectionOn id _ _ -> fex lst id >>= \case 
+                SelectionOn _ itm _ -> fex itm >>= \case 
                                         True  -> setFocusAttribute lst (black `on` red) 
                                         False -> setFocusAttribute lst (black `on` green)
-                _                  -> return ()
-    let deftAttr ls = fex lst 0 >>= \case 
-                                    True  -> setFocusAttribute ls (black `on` red)
-                                    False -> setFocusAttribute ls (black `on` green)
+                _                   -> return ()
 
     ------generate ui----------
     lui <- centered =<< hFixed 80 lst
@@ -57,51 +55,45 @@ main = do
     chgdl <- addToCollection c dui dfcg
     
     ---------create infor screen-----------
-    ifsfg <- newFocusGroup
-    chgif <- forM vdlst (\vid -> do
-                            infw <- plainText $ crtInfPg vid
-                            bnfw <- centered infw
-                            addToFocusGroup ifsfg bnfw
-                            addToCollection c bnfw ifsfg)
-    ifsfg `onKeyPressed` (\_ key _ -> if key == KLeft     then chgls >> return True  else return False)
 
     -------exit when q is pressed-------
     lfg   `onKeyPressed` (\_ key _ -> if key == KChar 'q' then exitSuccess else return False)
     dfcg  `onKeyPressed` (\_ key _ -> if key == KChar 'q' then exitSuccess else return False)
-    ifsfg `onKeyPressed` (\_ key _ -> if key == KChar 'q' then exitSuccess else return False)
 
     ------define activate process--------
     
-    let lnch = do
-          res <- getSelected lst
-          case res of
-            Nothing -> return False
-            Just (ind, _) -> fex lst ind >>= \case
-                                        True  -> chgdl >> return False 
-                                        False -> playVid (vdlst!!ind) >> return True
+                                            
     dlg `onDialogCancel` const exitSuccess
     dlg `onDialogAccept` (\_ ->  do
-                             res <- getSelected lst
-                             case res of
-                               Nothing       -> return ()
-                               Just (ind, _) -> playVid (vdlst!!ind) >> exitSuccess
+                             Just (_, (itm, _)) <- getSelected lst
+                             playVid itm
                          )
-    -------------------
-    let showInfor = do
-          now <- getSelected lst
-          case now of
-            Nothing -> return ()
-            Just (ind, _) -> chgif!!ind >> return () -- chgif is a list of IO action chg to inf
-    let retnToLst = chgls
-    ---------lst--------
-    lst `onKeyPressed` (\_ key _ -> if key == KEnter then lnch >>= (\c -> if c then exitSuccess
-                                                                               else return True)
-                                                      else return False)
 
-    lst `onKeyPressed` (\_ key _ -> if key == KRight then showInfor >> return True else return False)
+    ---------lst--------
+    lst `onKeyPressed` (\_ key _ -> case key of
+                            KEnter -> do
+                                Just (_, (itm, _)) <- getSelected lst
+                                fex itm >>= \case 
+                                    True  -> chgdl
+                                    False -> playVid itm
+                                return True
+                            _      -> return False
+                       )
+    ifsfg <- newFocusGroup
+    ifsfg `onKeyPressed` (\_ key _ -> if key == KLeft then chgls >> return True else return False)
+    ifsfg `onKeyPressed` (\_ key _ -> if key == KChar 'q' then exitSuccess else return False)
+
+    lst `onKeyPressed` (\_ key _ -> case key of
+                            KRight -> do
+                                Just (_, (itm, _)) <- getSelected lst
+                                inf <- centered =<< (plainText $ crtInfPg itm)
+                                addToFocusGroup ifsfg inf
+                                chgif <- addToCollection c inf ifsfg
+                                chgif
+                                return True
+                            _ -> return False)
 
     lst `onSelectionChange` schg
-    deftAttr lst
 
     ------evaluate real list--------
     schedule $ do
@@ -111,17 +103,17 @@ main = do
             when (length newVdlst < 1) $ error "empty list!"
             clearList lst
             forM_ newVdlst $ \v -> do
-                addToList lst (vidName v) =<< plainText (beaut v)
+                addToList lst v =<< plainText (beaut v)
         return ()
 
     runUi c $ defaultContext {normalAttr = white `on` black, 
                               focusAttr  = black `on` green
                              }
 
-fex lst ind = do
-    Just (str, _) <- getListItem lst ind
+fex itm = do
+    --Just (_, (itm, _)) <- getSelected lst
     locd <- fmap T.unpack getLocaldir
-    doesFileExist $ locd </> (T.unpack str)
+    doesFileExist $ locd </> (T.unpack . vidName $ itm)
 
 crtInfPg :: Video -> T.Text
 crtInfPg vid = T.unlines ["", "",
