03ceaac9e459daab68617244166733c55e76b0ff
sifmelcara <rbdn@9mm.davy.tw>
2015-01-23 15:30:26 +0800

    write Storing func

---
+++ src/Log.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Log where
+
+import Video
+import Data.Binary
+import System.Directory
+import System.FilePath
+import Control.Applicative ((<$>))
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import Data.Binary
+import qualified Data.ByteString.Lazy as B
+
+instance Binary T.Text where
+    put = put . T.encodeUtf8
+    get = T.decodeUtf8 <$> get
+
+instance Binary Video where
+    put vid = do
+        put $ vidName vid
+    get = do
+        name <- get
+        return Video { vidName = name
+                     , vidLink = ""
+                     , vidDate = ""
+                     , vidSize = ""
+                     }
+
+logname = ".pgdl.cache"
+
+writeVid :: [Video] -> IO ()
+writeVid vs = do
+    hdir <- getHomeDirectory
+    B.writeFile (hdir </> logname) $ encode vs  
+
+readVid :: IO [Video]
+readVid = do
+    hdir <- getHomeDirectory
+    dat <- B.readFile (hdir </> logname)
+    return $ decode dat
