1780f5d3c0a5ff6996952d35bfdf0ea9207d4f95
sifmelcara <rbdn@9mm.davy.tw>
2015-02-04 11:29:51 +0800

    use pattern matching when calling func

--- src/Beaut.hs
+++ src/Beaut.hs
@@ -8,14 +8,13 @@ import Data.List
 import qualified Data.Text as T
 
 beaut :: Video -> T.Text
-beaut vid
+beaut (Video str _ _ _)
     | length dat < 3 = "\n" `app` str
     | otherwise      = T.intercalate "\n" [tb 4 `app` sbt, 
                                            tb 10 `app` nme, 
                                            tb 30 `app` eps
                                           ]
-    where str = vidName vid
-          dat = spl str
+    where dat = spl str
           (sbt:nme:eps:_) = dat
           tb n = T.replicate n " "
           app s1 s2 = s1 `T.append` s2
--- src/GenStat.hs
+++ src/GenStat.hs
@@ -9,16 +9,13 @@ import TestExist
 import qualified Data.Text as T
 
 genStat :: Video -> IO T.Text
-genStat vid = do
+genStat Video name _ size date = do
     st <- toStr $ downloaded name
     return $ (spc 3) `app` date `app`
              (spc 3) `app` size `app`
              (spc 20) `app` st `app`
              (spc 30)
-    where date = vidDate vid
-          size = vidSize vid
-          name = vidName vid
-          app = T.append
+    where app = T.append
           spc :: Int -> T.Text
           spc i = T.replicate i " "
 
@@ -26,4 +23,5 @@ toStr :: IO Bool -> IO T.Text
 toStr b = b >>= \case
     True -> return "Downloaded"
     _    -> return "Not Downloaded"
+
 
