45af3db84d09f94a37ceb64dd98aa09c89525b62
sifmelcara <rbdn@9mm.davy.tw>
2015-02-07 20:12:07 +0800

    use HTTP package instead of conduit

--- src/FetchHtml.hs
+++ src/FetchHtml.hs
@@ -4,31 +4,20 @@ module FetchHtml where
   
 import Getconfig
 
-import Control.Monad.IO.Class 
-import Control.Monad.Trans.Resource 
+import Network.HTTP
+import Network.URI
 import Data.Text.Encoding
+import Data.ByteString
 import Data.Maybe
-import Network.HTTP.Conduit
-
 import qualified Data.Text as T
-import qualified Data.ByteString.Lazy.Char8 as LC
-
-genReq :: IO Request
-genReq = do
-    username <- getUsername
-    password <- getPassword
-    servpath <- getServpath
-    case username of
-        "" -> return $ fromJust . parseUrl $ "http://" ++ T.unpack servpath
-        _  -> return $ applyBasicAuth (tob username) (tob password) . fromJust . parseUrl $ "http://" ++ T.unpack servpath
-    where tob = encodeUtf8
 
 fetchHtml :: IO T.Text
 fetchHtml = do
-    rq <- genReq
-    runResourceT $ do
-        mgr <- liftIO $ newManager conduitManagerSettings
-        res <- httpLbs rq mgr
-        return . decodeUtf8 . LC.toStrict $ responseBody res
-
+    usrn <- getUsername
+    usrp <- getPassword
+    servp <- getServpath
+    let uri = "http://" ++ usrn ++ ":" ++ usrp ++ "@" ++ servp
+    res <- simpleHTTP . mkRequest GET . fromJust . parseURI $ uri
+    cnt <- getResponseBody res
+    return $ decodeUtf8 cnt
 
