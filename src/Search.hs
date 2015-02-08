2a271e542f31d5b41acc810425f3ff818c8e7513
sifmelcara <rbdn@9mm.davy.tw>
2015-02-08 12:57:10 +0800

    rewrite search func

--- src/Search.hs
+++ src/Search.hs
@@ -9,8 +9,13 @@ import Data.Function
 import qualified Data.Text as T
 
 search :: [Video] -> [String] -> [Video]
+search vids par = filter pred vids
+    where pred v = all (\tar -> name `T.isInfixOf` (T.pack tar)) par
+            where name = vidName v
+
+{-
 search vlst param = foldl (\vs ps -> filter (targ ps . vidName) vs) vlst pt
     where pt = map T.pack param
           targ = T.isInfixOf `on` T.toUpper
-
+-}
 
