e65a02b218e9cfa18e70964f2f27233225d55bbc
sifmelcara <rbdn@9mm.davy.tw>
2015-02-11 06:42:24 +0800

    finish AskScene.hs

--- src/AskScene.hs
+++ src/AskScene.hs
@@ -5,21 +5,47 @@ module AskScene where
 import Graphics.Vty.Widgets.All
 
 import qualified Data.Text as T
+import Control.Monad
 
-data AskScene = AskScene { SceneWidget :: Widget Bordered
+data AskScene = AskScene { sceneWidget :: Widget (Bordered Padded)
+                         , playHand :: Handlers AskScene
+                         , downHand :: Handlers AskScene
+                         , quitHand :: Handlers AskScene
                          }
 
-newAskScene :: IO (Widget AskScene)
+newAskScene :: IO (AskScene, Widget FocusGroup)
 newAskScene = do
-    playB <- newBotton "Play it"
-    downB <- newBotton "Download and Play"
-    quitB <- newBotton "Quit"
-    buttonBox <- (return playB) <++> 
-                 (return downB) <++>
-                 (return quitB)
-    b <- (return $ plainText "What to do?") <--> buttonBox
+    playB <- newButton "Play it"
+    downB <- newButton "Download and Play"
+    quitB <- newButton "Quit"
+    buttonBox <- (mkwIO playB) <++> (mkwIO downB) <++> (mkwIO quitB)
+    b <- withPadding (padAll 1) =<< (plainText "What to do?") <--> (return buttonBox)
     fg <- newFocusGroup
     mapM_ (addToFocusGroup fg . buttonWidget) [playB, downB, quitB]
     ui <- withBorderedLabel "File Exists!" =<< bordered b
+    [phs, dhs, qhs] <- replicateM 3 newHandlers
+    let sce = AskScene { sceneWidget = ui
+                       , playHand = phs
+                       , downHand = dhs
+                       , quitHand = qhs
+                       }
+    onButtonPressed playB $ \_ -> 
+        fireEvent sce (return . playHand) sce
+    onButtonPressed downB $ \_ -> 
+        fireEvent sce (return . downHand) sce
+    onButtonPressed quitB $ \_ -> 
+        fireEvent sce (return . quitHand) sce
+    return (sce, fg)
+    where mkwIO = return . buttonWidget
+
+onScePlay :: AskScene -> Handler AskScene -> IO ()
+onScePlay = addHandler (return . playHand)
+
+onSceDown :: AskScene -> Handler AskScene -> IO ()
+onSceDown = addHandler (return . downHand)
+
+onSceQuit :: AskScene -> Handler AskScene -> IO ()
+onSceQuit = addHandler (return . quitHand)
+
 
 
