d699c9eab8b3a81300145932c5754210f986b954
sifmelcara <rbdn@9mm.davy.tw>
2015-03-03 03:13:02 +0800

    implement case insensitive search

--- src/Search.hs
+++ src/Search.hs
@@ -4,11 +4,12 @@ module Search where
 
 import Video
 
-
+import Data.Function
 import qualified Data.Text as T
 
 search :: [Video] -> [String] -> [Video]
 search vids par = filter ok vids
-    where ok v = all (\tar -> T.pack tar `T.isInfixOf` name) par
+    where ok v = all (\tar -> (T.pack tar) `ifx` name) par
             where name = if isVid v then vidName v else fldName v
+                  ifx = T.isInfixOf `on` T.toCaseFold
 
