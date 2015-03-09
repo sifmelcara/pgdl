9bf844f741a31bab6ad4f860fa0b6ed5d23ffe50
sifmelcara <rbdn@9mm.davy.tw>
2015-03-09 23:53:30 +0800

    fix hlint warning

--- src/Search.hs
+++ src/Search.hs
@@ -9,7 +9,7 @@ import qualified Data.Text as T
 
 search :: [Video] -> [String] -> [Video]
 search vids par = filter ok vids
-    where ok v = all (\tar -> (T.pack tar) `ifx` name) par
+    where ok v = all (\tar -> T.pack tar `ifx` name) par
             where name = if isVid v then vidName v else fldName v
                   ifx = T.isInfixOf `on` T.toCaseFold
 
