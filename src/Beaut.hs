64060fe50b462c57eaad5489928480569ac3eba0
sifmelcara <rbdn@9mm.davy.tw>
2015-03-11 00:28:48 +0800

    seperate pattern split algo

--- src/Beaut.hs
+++ src/Beaut.hs
@@ -6,22 +6,34 @@ import Video
 
 import qualified Data.Text as T
 
+-- | the video name parsing is only for specific type of videos...( [subtitle][video name][episode].... )
+-- for most filename, the parsing function will just return the video name itself.
+
 beaut :: Video -> T.Text
 beaut (Video str _ _ _) = beautT str
 beaut (Folder str _ _)  = beautT str
 
 beautT :: T.Text -> T.Text
-beautT str
-    | length dat < 3 = "\n" `app` str
-    | otherwise      = T.intercalate "\n" [tb 4 `app` sbt, 
-                                           tb 10 `app` nme, 
-                                           tb 30 `app` eps
-                                          ]
-    where dat = spl str
-          (sbt:nme:eps:_) = dat
-          tb n = T.replicate n " "
-          app s1 s2 = s1 `T.append` s2
-          spl = filter (\t -> T.length t /= 0) . getTags ["[", "]"] . return
-          getTags dl s = foldl (\ls d -> concatMap (T.splitOn d) ls) s dl
+beautT str = case cutName str of
+    Left x -> "\n" `apd` x
+    Right [sbt, nam, eps] -> T.unlines $ [ ws 4  `apd` sbt
+                                         , ws 10 `apd` nam
+                                         , ws 30 `apd` eps
+                                         ]
+    where apd = T.append
+          ws t = T.replicate t " "
 
+cutName :: T.Text -> Either T.Text [T.Text]
+cutName str
+    | length dat < 3 = Left str
+      -- parsing failure
+    | otherwise      = Right [sbtitle, name, episode]
+      -- ok.
+    where (sbtitle:name:episode:_) = dat
+          dat = filter (not . T.null) . getTags ["[", "]"] $ [str]
+          getTags :: [T.Text] -> -- ^ the delimiters
+                     [T.Text] -> -- ^ initial patterns
+                     [T.Text]
+          getTags dl s = foldl (\ls d -> concatMap (T.splitOn d) ls) s dl
+    
 
