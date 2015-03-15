70db24a415548ca6eed2e8ccbf8eb01c503d3788
sifmelcara <rbdn@9mm.davy.tw>
2015-03-15 12:39:59 +0800

    move some code to UI

--- src/UI.hs
+++ src/UI.hs
@@ -6,3 +6,26 @@ module UI where
 import Graphics.Vty
 import Graphics.Vty.Widgets.All
 
+tryExit _ key _ = case key of
+    KChar 'q' -> exitSuccess
+    _         -> return False
+
+fex itm
+    | isVid itm = downloaded $ vidName itm
+    | otherwise = return False
+
+-- return the videos in the list
+getListVideos lst = do
+    sz <- getListSize lst
+    forM [0..sz-1] $ \idx -> do
+        Just (itm, _) <- getListItem lst idx
+        return itm
+
+-- set lst content to vs
+setListVideos lst vs = do
+    Just (_, (oldItm, _)) <- getSelected lst
+    clearList lst 
+    forM_ vs $ \v -> addToList lst v =<< plainText (beaut v)
+    listFindFirst lst oldItm >>= \case
+        Just ind -> setSelected lst ind
+        Nothing  -> return ()
