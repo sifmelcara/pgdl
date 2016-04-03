f7669f03b373928e55ac149e2d88148c020a7cea
mingchuan <ming@culpring.com>
2016-04-04 02:43:26 +0800

    implemented the search bar

--- src/Main.hs
+++ src/Main.hs
@@ -20,6 +20,7 @@ import qualified Brick.Types as T
 import qualified Brick.Widgets.Border as B
 import qualified Brick.Widgets.List as L
 import qualified Brick.Widgets.Center as C
+import qualified Brick.Widgets.Edit as E
 import qualified Brick.AttrMap as A
 import qualified Data.Vector as V
 import Brick.Types
@@ -45,12 +46,15 @@ import System.Environment
 import Cache
 import Types
 
-drawUI :: LState -> [Widget]
-drawUI (LState _ l) = [C.hCenter . hLimit 80 $ vBox [entryList, statusBar]]
+-- | use cropping to draw UI in the future?
+drawUI :: MainState -> [Widget]
+drawUI mainState = case mainState of
+        (LState _ l) -> [C.hCenter . hLimit 80 $ vBox [entryList l, statusBar l]]
+        (SearchState _ l e) -> [C.hCenter . hLimit 80 $ vBox [entryList l, undefined]]
     where
     -- note: the vertical size of the list is somewhat strange when the hroizontal size limit
     -- is not the multiple of its element size (it is 3)
-    entryList = L.renderList l listDrawElement
+    entryList lst = L.renderList lst listDrawElement
     listDrawElement False (Directory a _) = C.hCenter . txt . mid . strip80 $ decodedName a 
     listDrawElement False (File a _) = C.hCenter . txt . mid . strip80 $ decodedName a 
     listDrawElement True d@(Directory _ _) = withAttr "directory" $ listDrawElement False d
@@ -76,9 +80,9 @@ drawUI (LState _ l) = [C.hCenter . hLimit 80 $ vBox [entryList, statusBar]]
             accumFst = scanl (\(l, _) r -> (l + charDisplayLen r, r)) (0, ' ')
 
 
-    statusBar = withAttr "statusBar" . str . expand $ info
+    statusBar lst = withAttr "statusBar" . str . expand $ info lst
     expand s = s ++ replicate 88 ' '
-    info = case L.listSelectedElement l of
+    info lst = case L.listSelectedElement lst of
             Nothing -> "Nothing selected by user"
             Just (_, sel) -> "  " ++ show (lastModified entry) ++ "    " ++ maybe "Nothing" friendlySize (fileSize entry)
                 where
@@ -87,8 +91,10 @@ drawUI (LState _ l) = [C.hCenter . hLimit 80 $ vBox [entryList, statusBar]]
                             File entry _ -> entry
     
 
--- |                 father  contents
-data LState = LState LState (L.List DNode)
+-- |                 father  contents                 
+data MainState = LState MainState (L.List DNode)
+               | SearchState MainState (L.List DNode) E.Editor
+               | SortSelectionState MainState
 
 main = do
     let askUserServpath = undefined
@@ -107,7 +113,7 @@ main = do
                 putStrLn "(you can use --offline to browse the webpages you load last time)"
                 fetch rootUrl
     let
-        initialState :: LState
+        initialState :: MainState
         initialState = LState initialState lst
             where
             lst = L.list (T.Name "root") (V.fromList dNodes) 3
@@ -119,7 +125,7 @@ main = do
                   , M.appAttrMap = const theMap 
                   , M.appLiftVtyEvent = id
                   }
-        appEvent :: LState -> V.Event -> T.EventM (T.Next LState)
+        appEvent :: MainState -> V.Event -> T.EventM (T.Next MainState)
         appEvent ls@(LState father lst) e = case e of
             V.EvKey V.KEsc [] -> M.halt ls
             V.EvKey (V.KChar 'q') [] -> M.halt ls
@@ -139,7 +145,28 @@ main = do
             V.EvKey V.KRight [] -> case L.listSelectedElement lst of
                                     Nothing -> M.continue $ ls
                                     Just (_, sel) -> M.suspendAndResume $ entryAttrViewer sel >> return ls
+            V.EvKey (V.KChar '/') [] -> M.continue $ SearchState ls lst (E.editor "keyword" (str.unlines) (Just 1) "")
+            --                                                      ^ current list which reactively change with editor
             ev -> M.continue =<< (LState father <$> (T.handleEvent ev lst))
+        appEvent ss@(SearchState ms@(LState _ origLst) lst ed) e = case e of
+            V.EvKey V.KEsc [] -> M.halt ss
+            V.EvKey V.KEnter [] -> M.continue $ LState ms lst
+            ev@(V.EvKey (V.KChar _) []) -> do
+                newEd <- T.handleEvent ev ed
+                -- | update the list, lst
+                let 
+                    linesToALine [l] = l
+                    linesToALine _ = error "not one line of words in the search bar, why?"
+                    -- | Why does L.listReplace require an Eq instance of Vector elements...?
+                    applyFilter kw lst = replaceList newElms lst
+                        where
+                        newElms = V.filter (\dn -> kw `isKeyWordOf` (getDNText dn)) $ L.listElements lst
+                        replaceList es l = l {L.listElements = es}
+                    getDNText (Directory e _) = decodedName e
+                    getDNText (File e _) = decodedName e 
+                    isKeyWordOf t1 t2 = T.toCaseFold t1 `T.isInfixOf` T.toCaseFold t2
+                M.continue $ SearchState ms (applyFilter (T.pack . linesToALine $ E.getEditContents newEd) origLst) newEd
+            ev -> M.continue ss
         theMap = A.attrMap V.defAttr [ (L.listAttr, V.white `on` V.black)
 --                                     , (L.listSelectedAttr, V.black `on` V.cyan)
                                      , ("directory", V.black `on` V.magenta)
