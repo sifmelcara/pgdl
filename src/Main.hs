b5dde88d2ffd9089389b44906a6ec1db0db3ac28
mingchuan <ming@culpring.com>
2016-05-01 00:30:45 +0800

    syntax clean up

--- src/Main.hs
+++ src/Main.hs
@@ -1,54 +1,44 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
+
 module Main
 where
 
-import Debug.Trace
-import Fetcher
 import qualified Data.Text as T
+import qualified Data.Vector as V
 import Data.Text (Text)
-import qualified Data.ByteString as B
-import Data.Text.Encoding
-import Networking
+import Data.Maybe
 import DownloadInterface
 import Control.Monad
 import Control.Monad.IO.Class
 import System.FilePath
+import System.Environment
+import Text.HTML.DirectoryListing.Type
 
 import qualified Graphics.Vty as V
 import qualified Brick.Main as M
 import qualified Brick.Types as T
-import qualified Brick.Widgets.Border as B
 import qualified Brick.Widgets.List as L
 import qualified Brick.Widgets.Center as C
 import qualified Brick.Widgets.Edit as E
 import qualified Brick.AttrMap as A
-import qualified Data.Vector as V
-import Brick.Types
-  ( Widget
-  )
+import Brick.Types (Widget)
 import Brick.Widgets.Core
-  ( (<+>)
-  , str
-  , vLimit
-  , hLimit
-  , vBox, hBox
-  , withAttr
-  , forceAttr
-  , txt
-  )
-import Brick.Util (fg, on)
+import Brick.Util (on)
 
-import Text.HTML.DirectoryListing.Type
-import Data.Maybe
 import EntryAttrViewer
 import Utils
 import Configure
-import System.Environment
 import Cache
 import Types
-import Local
+import Fetcher
 import qualified Utils as U
+
+-- |                    father            contents                 
+data MainState = LState (Maybe MainState) (L.List DNode)
+               | SearchState MainState (L.List DNode) E.Editor
+
 
 -- | use cropping to draw UI in the future?
 drawUI :: MainState -> [Widget]
@@ -70,11 +60,12 @@ drawUI mainState = case mainState of
     listDrawElement True d@(Directory _ _) = withAttr "directory" $ listDrawElement False d
     listDrawElement True f@(File _ _ False) = withAttr "file" $ listDrawElement False f
     listDrawElement True f@(File _ _ True) = withAttr "downloaded file" $ listDrawElement False f
-    mid s = T.unlines $ ["", s, ""]
+    mid s = T.unlines ["", s, ""]
     stripWidth :: Text -> Text
     stripWidth t = case U.cutTextByDisplayLength (U.terminalWidth-5) t of
+                    [] -> ""
                     [singleLine] -> singleLine
-                    (x:xs) -> x `T.append` "..."
+                    (x:_) -> x `T.append` "..."
 
     searchBar ed = forceAttr "searchBar" $ hBox [txt "search: ", E.renderEditor ed]
 
@@ -87,13 +78,8 @@ drawUI mainState = case mainState of
                 entry = case sel of
                             Directory e _ -> e
                             File e _ _ -> e
-    
 
--- |                    father            contents                 
-data MainState = LState (Maybe MainState) (L.List DNode)
-               | SearchState MainState (L.List DNode) E.Editor
-               | SortSelectionState MainState
-
+main :: IO ()
 main = do
     let askUserServpath = undefined
     rootUrl <- getArgs >>= \case
@@ -101,15 +87,14 @@ main = do
                 _ -> getServpath >>= \case
                         Nothing -> askUserServpath
                         Just p -> return p
-    dNodes <- do
-        getArgs >>= \case
-            ["--offline"] -> readCache >>= \case
-                Nothing -> error "no offline data or data corrupted."
-                Just dlst -> return dlst
-            _ -> do
-                putStrLn "loading webpage..."
-                putStrLn "(you can use --offline to browse the webpages you load last time)"
-                fetch rootUrl
+    dNodes <- getArgs >>= \case
+                ["--offline"] -> readCache >>= \case
+                    Nothing -> error "no offline data or data corrupted."
+                    Just dlst -> return dlst
+                _ -> do
+                    putStrLn "loading webpage..."
+                    putStrLn "(you can use --offline to browse the webpages you load last time)"
+                    fetch rootUrl
     let
         initialState :: MainState
         initialState = LState Nothing lst
@@ -128,7 +113,7 @@ main = do
             V.EvKey V.KEsc [] -> M.halt ls
             V.EvKey (V.KChar 'q') [] -> M.halt ls
             V.EvKey V.KEnter [] -> case L.listSelectedElement lst of
-                                    Nothing -> M.continue $ ls
+                                    Nothing -> M.continue ls
                                     Just (rowNum, child) -> case child of
                                         Directory entry dnsOp -> do
                                             dns <- liftIO dnsOp -- grab the subdirectory
@@ -137,7 +122,7 @@ main = do
                                             let fn = decodedName entry
                                             path <- liftIO $ getLocaldir >>= \case
                                                                 Nothing -> return fn 
-                                                                Just pre -> return $ T.pack ((T.unpack pre) </> (T.unpack fn))
+                                                                Just pre -> return $ T.pack (T.unpack pre </> T.unpack fn)
                                             let
                                                 dui = downloadInterface url path (fromJust $ fileSize entry) False
                                                 --                                ^ not good, unsafe
@@ -150,17 +135,17 @@ main = do
                                             let fn = decodedName entry
                                             path <- liftIO $ getLocaldir >>= \case
                                                                 Nothing -> return fn 
-                                                                Just pre -> return $ T.pack ((T.unpack pre) </> (T.unpack fn))
+                                                                Just pre -> return $ T.pack (T.unpack pre </> T.unpack fn)
                                             let dui = downloadInterface url path (fromJust $ fileSize entry) True
                                             --                                    ^ not good, unsafe
                                             M.suspendAndResume $ dui >> return ls
             V.EvKey V.KLeft [] -> M.continue $ fromMaybe ls father
             V.EvKey V.KRight [] -> case L.listSelectedElement lst of
-                                    Nothing -> M.continue $ ls
+                                    Nothing -> M.continue ls
                                     Just (_, sel) -> M.suspendAndResume $ entryAttrViewer sel >> return ls
             V.EvKey (V.KChar '/') [] -> M.continue $ SearchState ls lst (E.editor "searchBar" (str.unlines) (Just 1) "")
             --                                                      ^ current list which reactively change with editor
-            ev -> M.continue =<< (LState father <$> (T.handleEvent ev lst))
+            ev -> M.continue =<< (LState father <$> T.handleEvent ev lst)
         appEvent ss@(SearchState ms@(LState _ origLst) lst ed) e = case e of
             V.EvKey V.KEsc [] -> M.halt ss
             V.EvKey V.KEnter [] -> case E.getEditContents ed of
@@ -175,15 +160,15 @@ main = do
                     -- | Why does L.listReplace require an Eq instance of Vector elements...?
                     applyFilter kw lst = replaceList newElms lst
                         where
-                        newElms = V.filter (\dn -> kw `isKeyWordOf` (getDNText dn)) $ L.listElements lst
+                        newElms = V.filter (\dn -> kw `isKeyWordOf` getDNText dn) $ L.listElements lst
                         -- | sometime this will crash? how?
                         replaceList es l = l {L.listElements = es, L.listSelected = Just 0}
                     getDNText (Directory e _) = decodedName e
                     getDNText (File e _ _) = decodedName e 
                     isKeyWordOf t1 t2 = T.toCaseFold t1 `T.isInfixOf` T.toCaseFold t2
                 M.continue $ SearchState ms (applyFilter (T.pack . linesToALine $ E.getEditContents newEd) origLst) newEd
+        appEvent SearchState {} _ = error "unexpected prev state in SearchState."
         theMap = A.attrMap V.defAttr [ (L.listAttr, V.white `on` V.black)
---                                     , (L.listSelectedAttr, V.black `on` V.cyan)
                                      , ("directory", V.black `on` V.magenta)
                                      , ("file", V.black `on` V.cyan)
                                      , ("downloaded file", V.black `on` V.red)
