54724ca3c015b3ca3e65b1aaf4003209d79afba6
mingchuan <ming@culpring.com>
2016-06-07 02:37:06 +0800

    Types.hs: redundant imports clean up

--- src/Types.hs
+++ src/Types.hs
@@ -1,22 +1,10 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Types
 where
 
-import Control.Applicative ((<$>))
 import Text.HTML.DirectoryListing.Type
 import Data.Text (Text)
-import Data.Maybe
-import qualified Brick.Widgets.List as L
-import qualified Brick.Types as T
-import Brick.Widgets.List (listSelectedL, listElementsL, listSelectedElement)
-import qualified Data.Vector as V
-import Data.Vector ((!), Vector)
-import Data.Sequence (Seq, (><))
-import qualified Data.Sequence as S
-import Lens.Micro
-import Control.Monad
 
-data DNode = Directory Entry (IO [DNode]) | File Entry Text Bool
---                                                          downloaded?
+data DNode = Directory Entry (IO [DNode])
+           | File Entry Text Bool
+--                           ^ is the file downloaded ?
 
