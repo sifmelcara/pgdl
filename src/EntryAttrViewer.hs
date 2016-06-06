29c57288ace8a2305bd8c55d82db98b894c85d1a
mingchuan <ming@culpring.com>
2016-06-07 02:47:22 +0800

    use simpleMain instead of defaultMain for attribute viewer

--- src/EntryAttrViewer.hs
+++ src/EntryAttrViewer.hs
@@ -5,53 +5,18 @@ where
 
 import Data.Text (Text)
 import qualified Data.Text as T
-
-import qualified Graphics.Vty as V
 import qualified Brick.Main as M
-import qualified Brick.Types as T
-import qualified Brick.Widgets.ProgressBar as P
 import qualified Brick.Widgets.Center as C
 import qualified Brick.Widgets.Core as C
-import qualified Brick.AttrMap as A
-import Brick.Types (Widget)
-import Brick.Widgets.Core
-import Brick.Util (on)
 
-import Types
 import Text.HTML.DirectoryListing.Type
+import Types
 import qualified Utils as U
 
 entryAttrViewer :: DNode -> IO ()
--- | Temporary use File to process Directory...
 entryAttrViewer (Directory entry _) = entryAttrViewer (File entry "directory no link." False)
-entryAttrViewer (File entry url downloaded) = do
-    let
-        initialState :: () 
-        initialState = ()
-        theApp =
-            M.App { M.appDraw = drawUI
-                  , M.appChooseCursor = M.neverShowCursor
-                  , M.appHandleEvent = appEvent
-                  , M.appStartEvent = return
-                  , M.appAttrMap = const theMap 
-                  , M.appLiftVtyEvent = id
-                  }
-        appEvent :: () -> V.Event -> T.EventM String (T.Next ())
-        appEvent () e = case e of
-            V.EvKey V.KEsc [] -> M.halt ()
-            V.EvKey (V.KChar 'q') [] -> M.halt ()
-            V.EvKey V.KLeft [] -> M.halt ()
-            _ -> M.continue ()
-        theMap = A.attrMap V.defAttr [ (P.progressCompleteAttr, V.black `on` V.cyan)
-                                     , (P.progressIncompleteAttr, V.black `on` V.white)
-                                     ]
-        drawUI :: () -> [Widget String]
-        drawUI _ = [ui]
-            where
-            ui = C.vCenter . C.hCenter .
-                 C.hLimit U.terminalWidth . C.txt $ info
-    M.defaultMain theApp initialState
-    return ()
+entryAttrViewer (File entry url downloaded) =
+    M.simpleMain . C.vCenter . C.hCenter . C.hLimit U.terminalWidth . C.txt $ info
     where
     info = T.unlines $
            zipWith (\describ s -> T.intercalate "\n" . 
