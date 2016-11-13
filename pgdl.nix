b46d8d7ed3c56a8786fa8944e198524f41093956
mingchuan <ming@culpring.com>
2016-11-13 17:33:01 +0800

    bump version

--- pgdl.nix
+++ pgdl.nix
@@ -12,7 +12,7 @@ let
       }:
       mkDerivation {
         pname = "pgdl";
-        version = "10.3";
+        version = "10.4";
         src = ./.;
         isLibrary = false;
         isExecutable = true;
